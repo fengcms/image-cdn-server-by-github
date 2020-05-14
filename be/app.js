ae97e95c7cf31caebd6a54493ac79f6860a8745b
FungLeo <web@fengcms.com>
2020-05-14 20:34:00 +0800

    添加配置，调整代码

--- be/app.js
+++ be/app.js
@@ -1,19 +1,22 @@
 const express = require('express')
 const formidableMiddleware = require('express-formidable')
-const fs = require('fs')
-const path = require('path')
 
 const app = express()
 app.use(formidableMiddleware())
 
+console.log(global.config)
+
 app.post('/upload', (req, res) => {
   res.json({ path: 'xxx', status: 0 })
 })
-
-app.listen(
-  17680,
-  '0.0.0.0',
-  () => {
-    console.log(`Simple mock listening on http://${host}:${port}!`)
-  }
-)
+app.use((req, res) => {
+  res.json({ status: 404 }, 404)
+})
+module.exports = app
+// app.listen(
+//   17680,
+//   '0.0.0.0',
+//   () => {
+//     console.log(`Simple mock listening on http://${host}:${port}!`)
+//   }
+// )
---
+++ gui.js
@@ -0,0 +1,36 @@
+// Modules to control application life and create native browser window
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+let mainWindow
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  // and load the index.html of the app.
+  mainWindow.loadFile('index.html')
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+module.exports = 'gui'
