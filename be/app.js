04af3b10be961b361458e9b00983a9bb10fcf65f
FungLeo <web@fengcms.com>
2020-05-14 18:10:11 +0800

    init

---
+++ be/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const formidableMiddleware = require('express-formidable')
+const fs = require('fs')
+const path = require('path')
+
+const app = express()
+app.use(cookieParser())
+app.use(formidableMiddleware())
+
+app.post('/upload', (req, res) => {
+  res.json({path:'xxx', status: 0})
+})
+
+app.listen(
+  17680,
+  0.0.0.0,
+  () => {
+    console.log(`Simple mock listening on http://${host}:${port}!`)
+  }
+)
