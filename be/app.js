61439e05d4bf9b5add8b0a1ac5fa579d42c99478
FungLeo <web@fengcms.com>
2020-05-14 18:15:43 +0800

    添加 eslint 代码校验

--- be/app.js
+++ be/app.js
@@ -4,16 +4,15 @@ const fs = require('fs')
 const path = require('path')
 
 const app = express()
-app.use(cookieParser())
 app.use(formidableMiddleware())
 
 app.post('/upload', (req, res) => {
-  res.json({path:'xxx', status: 0})
+  res.json({ path: 'xxx', status: 0 })
 })
 
 app.listen(
   17680,
-  0.0.0.0,
+  '0.0.0.0',
   () => {
     console.log(`Simple mock listening on http://${host}:${port}!`)
   }
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow } = require('electron')
 const path = require('path')
 let mainWindow
 function createWindow () {
@@ -42,5 +42,5 @@ app.on('window-all-closed', function () {
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
 try {
-  require('electron-reloader')(module,{});
+  require('electron-reloader')(module, {})
 } catch (_) {}
