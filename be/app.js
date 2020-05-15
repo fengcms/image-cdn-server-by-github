659635a68eaa9b24248837aefa1db8b9382d6c3c
FungLeo <web@fengcms.com>
2020-05-15 12:28:27 +0800

    上传接口完成

--- be/app.js
+++ be/app.js
@@ -1,22 +1,77 @@
-const express = require('express')
-const formidableMiddleware = require('express-formidable')
+const Koa = require('koa')
+const koaJson = require('koa-json')
+const koaBody = require('koa-body')
+const toType = require('to-type')
+const fs = require('fs')
+const Path = require('path')
+const Images = require('images')
 
-const app = express()
-app.use(formidableMiddleware())
+const { calcFileExt, calcSavePath } = require('./util')
 
-console.log(global.config)
+const app = new Koa()
+app.use(koaBody({
+  multipart: true,
+  parsedMethods: ['POST'],
+  formidable: {
+    hash: 'md5',
+    maxFieldsSize: 2 * 1024 * 1024 * 100,
+    maxFileSize: 2 * 1024 * 1024 * 100,
+    keepExtensions: false
+  },
+  onError (_, ctx) {
+    ctx.throw(413, '文件超过限制大小！')
+  }
+}))
+app.use(koaJson({ pretty: false, param: 'pretty' }))
 
-app.post('/upload', (req, res) => {
-  res.json({ path: 'xxx', status: 0 })
+app.use(async (ctx, next) => {
+  const { files, type } = ctx.request
+  if (type !== 'multipart/form-data' || !files) {
+    ctx.throw(400, '请求类型错误，上传文件接口仅支持 multipart/form-data')
+  }
+  const { file } = files
+  if (!file) {
+    ctx.throw(400, '上传文件不能为空！')
+  }
+  if (toType(file) === 'array') {
+    file.forEach(i => {
+      fs.unlinkSync(i.path)
+    })
+    ctx.throw(400, '只支持单张图片上传')
+  }
+  if (ctx.path !== '/upload') {
+    ctx.throw(404)
+  }
+  await next()
 })
-app.use((req, res) => {
-  res.json({ status: 404 }, 404)
+
+app.use(async ctx => {
+  const { path, hash } = ctx.request.files.file
+  const fileExt = calcFileExt(path)
+  if (!fileExt) return ctx.throw(415)
+  const { saveDir, savePath, saveSourcePath, returnPath, returnSourcePath } = calcSavePath(hash, fileExt)
+  // 检查目录是否存在，不存在则创建
+  if (!fs.existsSync(saveDir)) fs.mkdirSync(saveDir)
+  // 非 jpg 图片直接保存
+  if (fileExt === '.jpg') {
+    const sourceImage = Images(path)
+    const w = sourceImage.width()
+    const h = sourceImage.height()
+    // 只有宽高均大于300，才加水印，否则直接保存
+    if (w > 300 && h > 300) {
+      const mark = Images(Path.resolve(__dirname, './mark.png'))
+      Images(sourceImage).draw(mark, w - 180, h - 60).save(savePath)
+      // 保存原图
+      fs.renameSync(path, saveSourcePath)
+      ctx.body = { path: returnPath, sourcePath: returnSourcePath }
+    } else {
+      fs.renameSync(path, savePath)
+      ctx.body = { path: returnPath }
+    }
+  } else {
+    fs.renameSync(path, savePath)
+    ctx.body = { path: returnPath }
+  }
 })
+
 module.exports = app
-// app.listen(
-//   17680,
-//   '0.0.0.0',
-//   () => {
-//     console.log(`Simple mock listening on http://${host}:${port}!`)
-//   }
-// )
---
+++ be/util.js
@@ -0,0 +1,48 @@
+const fs = require('fs')
+const path = require('path')
+const { imageSaveDir } = global.config
+const calcFileExt = path => {
+  let fileBuffer = fs.readFileSync(path)
+  // 将上文提到的 文件标识头 按 字节 整理到数组中
+  const imageBufferHeaders = [
+    { bufBegin: [0xff, 0xd8], bufEnd: [0xff, 0xd9], suffix: '.jpg' },
+    { bufBegin: [0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a], suffix: '.png' },
+    { bufBegin: [0x47, 0x49, 0x46, 0x38, 0x39, 0x61], suffix: '.gif' },
+    { bufBegin: [0x47, 0x49, 0x46, 0x38, 0x37, 0x61], suffix: '.gif' },
+    { bufBegin: [0x42, 0x4d], suffix: '.bmp' }
+  ]
+  for (const imageBufferHeader of imageBufferHeaders) {
+    let isEqual
+    // 判断标识头前缀
+    if (imageBufferHeader.bufBegin) {
+      const buf = Buffer.from(imageBufferHeader.bufBegin)
+      isEqual = buf.equals(
+        // 使用 buffer.slice 方法 对 buffer 以字节为单位切割
+        fileBuffer.slice(0, imageBufferHeader.bufBegin.length)
+      )
+    }
+    // 判断标识头后缀
+    if (isEqual && imageBufferHeader.bufEnd) {
+      const buf = Buffer.from(imageBufferHeader.bufEnd)
+      isEqual = buf.equals(fileBuffer.slice(-imageBufferHeader.bufEnd.length))
+    }
+    if (isEqual) {
+      fileBuffer = undefined
+      return imageBufferHeader.suffix
+    }
+  }
+  // 未能识别到该文件类型
+  fileBuffer = undefined
+  return ''
+}
+const calcSavePath = (hash, fileExt) => {
+  const saveDir = path.resolve(imageSaveDir, './image/', hash.substring(0, 2))
+  const savePathPre = path.resolve(saveDir, hash.substring(2))
+  const savePath = savePathPre + fileExt
+  const saveSourcePath = savePathPre + '_source' + fileExt
+  const returnPath = savePath.replace(imageSaveDir, '/')
+  const returnSourcePath = saveSourcePath.replace(imageSaveDir, '/')
+  return { saveDir, savePath, saveSourcePath, returnPath, returnSourcePath }
+}
+
+module.exports = { calcFileExt, calcSavePath }
